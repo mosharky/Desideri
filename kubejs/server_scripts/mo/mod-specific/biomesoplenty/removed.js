e940f626a0ffc569ab0029a45d3775b5314f6bb4
mo_shark <medoelzarkalt@gmail.com>
2022-01-24 11:51:08 -0500

    Cleaner regex on BoP removals

--- kubejs/server_scripts/mo/mod-specific/biomesoplenty/removed.js
+++ kubejs/server_scripts/mo/mod-specific/biomesoplenty/removed.js
@@ -1,12 +1,6 @@
 global.fullRemovals.push(
-    /biomesoplenty:.*willow*./,
-    /compatoplenty:.*willow*./,
-    /decorative_compat:.*willow*./,
+    /(biomesoplenty|compatoplenty|decorative_compat):.*willow*./,
+    /(biomesoplenty|compatoplenty|decorative_compat):.*magic*./,
+    /(biomesoplenty|compatoplenty|decorative_compat):.*umbran*./,
     'biomesoplenty:spanish_moss',
-    /biomesoplenty:.*magic*./,
-    /compatoplenty:.*magic*./,
-    /decorative_compat:.*magic*./,
-    /biomesoplenty:.*umbran*./,
-    /compatoplenty:.*umbran*./,
-    /decorative_compat:.*umbran*./,
 )
