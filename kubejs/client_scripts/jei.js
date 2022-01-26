f1c69a54c2f06255a1b2e1a62c903215f5de0225
mo_shark <medoelzarkalt@gmail.com>
2022-01-26 18:14:42 -0500

    Hid dyed variants of Refined Storage blocks

--- kubejs/client_scripts/jei.js
+++ kubejs/client_scripts/jei.js
@@ -1,4 +1,7 @@
 // Hide items in JEI here
-onEvent('jei.hide.items', event => {
-	//event.hide(global.fullRemovals)
+onEvent('jei.hide.items', jei => {
+	//jei.hide(global.fullRemovals)
+
+	// Hides Refined Storage's dyed variants except for red
+	jei.hide([/refinedstorage:(white|orange|magenta|yellow|lime|pink|gray|light_gray|cyan|purple|blue|brown|green||black)_(controller|creative_controller|grid|crafting_grid|pattern_grid|fluid_grid|network_receiver|network_transmitter|relay|wireless_transmitter|disk_manipulator|crafter|crafter_manager|crafting_monitor|security_manager|detector)/])
 })
