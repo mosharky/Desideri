6664ad39965848c7602b31f902bdbfc5bcf89b69
mo_shark <medoelzarkalt@gmail.com>
2022-01-27 22:05:51 -0500

    smol rework of clientside stuff

--- kubejs/client_scripts/clientside.js
+++ kubejs/client_scripts/clientside.js
@@ -1,6 +1,6 @@
-// Hide items in JEI here
+// Hide items in JEI item list
 onEvent('jei.hide.items', jei => {
-	//jei.hide(global.fullRemovals)
+	// jei.hide(global.fullRemovals)
 	jei.hide([
 		// Hides unused BoP wood
 		/(biomesoplenty|compatoplenty|decorative_compat|supplementaries):.*magic*./,
@@ -8,4 +8,14 @@ onEvent('jei.hide.items', jei => {
 		// Hides Refined Storage's dyed variants except for red
 		/refinedstorage:(white|orange|magenta|yellow|lime|pink|gray|light_gray|cyan|purple|blue|brown|green||black)_(controller|creative_controller|grid|crafting_grid|pattern_grid|fluid_grid|network_receiver|network_transmitter|relay|wireless_transmitter|disk_manipulator|crafter|crafter_manager|crafting_monitor|security_manager|detector)/,
 	])
+})
+
+onEvent('jei.information', jei => {
+	jei.add(global.fullRemovals, Text.of('DISABLED').red().bold())
+})
+
+// Item tooltips
+onEvent('item.tooltip', tooltip => {
+	tooltip.add(global.fullRemovals, Text.of('DISABLED').red().bold())
+	tooltip.add(/refinedstorage:red_/, Text.of('Can be any color').gray())
 })
