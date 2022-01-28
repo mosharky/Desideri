dbf96831c7a8907cd4ce973fe7cb1bf511a3edf7
mo_shark <medoelzarkalt@gmail.com>
2022-01-28 15:22:47 -0500

    2 Raw pasta output from mixing

--- kubejs/server_scripts/desideri/mod-specific/create/mixing.js
+++ kubejs/server_scripts/desideri/mod-specific/create/mixing.js
@@ -1,8 +1,3 @@
 onEvent('recipes', (event) => {
-    event.recipes.createMixing(
-        // Output
-        'farmersdelight:raw_pasta',
-        // Input
-        ['create:dough', '#forge:eggs']
-    ).id('hiraeth:create/mixing/raw_pasta')
+    event.recipes.createMixing('2x farmersdelight:raw_pasta', ['create:dough', '#forge:eggs']).id('hiraeth:create/mixing/raw_pasta')
 })
