56413e55aae7289e6c2a15b0050cb28f85ba74ac
mo_shark <medoelzarkalt@gmail.com>
2022-01-30 00:38:32 -0500

    Removed BoP Black Sand

--- kubejs/server_scripts/desideri/recipes/removals.js
+++ kubejs/server_scripts/desideri/recipes/removals.js
@@ -12,7 +12,8 @@ global.fullRemovals.push(
     'biomesoplenty:spanish_moss',
     /aquaculture:.*_fillet_knife/,
     /quark:(backpack|potato_crate|carrot_crate|beetroot_crate|chute|gravisand|abacus|dragon_scale|.*limestone*.|.*blossom*.)/,
-    /quark:(granite|andesite|diorite|calcite|dripstone|tuff)_pillar/
+    /quark:(granite|andesite|diorite|calcite|dripstone|tuff)_pillar/,
+    /biomesoplenty:.*black_sand*./
 )
 // Removes by recipe ID
 idRemovals.push(
