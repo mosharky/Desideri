8b9d7f75357d2d2ac9e1cb14ac317477eeb0e521
mo_shark <medoelzarkalt@gmail.com>
2022-03-16 16:29:36 -0400

    creative mode tab REI bug :/

--- kubejs/client_scripts/clientside.js
+++ kubejs/client_scripts/clientside.js
@@ -1,11 +1,17 @@
 // Hide items in REI item list
 onEvent('rei.hide.items', rei => {
 	// rei.hide(global.fullRemovals)
+	// blocks that aren't in creative menus aren't getting hidden?
+	// why are tables and beehives not in creative menus???
 	rei.hide([
-		/(biomesoplenty|compatoplenty|decorative_compat|supplementaries):(.*magic.*|.*umbran.*|.*palm.*)/, // tables and beehives aren't caught by this?
-		/refinedstorage:(?!red).+_(controller|creative_controller|.*grid.*|network_receiver|network_transmitter|relay|wireless_transmitter|disk_manipulator|crafter|crafter_manager|crafting_monitor|security_manager|detector)/,
+		// tables and beehives added by Compat O' Plenty aren't hidden by this?
+		/(biomesoplenty|compatoplenty|decorative_compat|supplementaries):(.*magic.*|.*umbran.*|.*palm.*)/,
+		// this works fine
+		/refinedstorage:(?!red).+_(controller|creative_controller|grid|crafting_grid|pattern_grid|fluid_grid|network_receiver|network_transmitter|relay|wireless_transmitter|disk_manipulator|crafter|crafter_manager|crafting_monitor|security_manager|detector)/,
+		// this doesn't work either
 		/biomesoplenty:potted.*/,
-		/lootr:(?!trophy).*/, // ???
+		// lootr blocks aren't being hidden
+		/lootr:(?!trophy).*/,
 	])
 })
 
