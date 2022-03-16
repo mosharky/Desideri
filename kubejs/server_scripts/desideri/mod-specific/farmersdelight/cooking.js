2fc5fa4197125494fd721d4194cb1c4a3446e3ce
mo_shark <medoelzarkalt@gmail.com>
2022-03-16 16:29:49 -0400

    Cooking function revamp

--- kubejs/server_scripts/desideri/mod-specific/farmersdelight/cooking.js
+++ kubejs/server_scripts/desideri/mod-specific/farmersdelight/cooking.js
@@ -1,23 +1,23 @@
-function cookingRecipe(result, experience, ingredient) {
-    return {
-        type: 'farmersdelight:cooking',
-        result: result,
-        experience: experience,
-        ingredients: ingredient,
-        cookingtime: 200
-    }
-}
-
+// No bowl inputs?
 onEvent('recipes', (event) => {
-    const recipes = [
-    cookingRecipe(Item.of('kubejs:hominy'), 0.35, [
-            Ingredient.of('corn_delight:corn_seeds'),
-            Ingredient.of('corn_delight:corn_seeds'),
-            Ingredient.of('kubejs:lye')
-        ])
-    ]
+    function cookingRecipe(result, ingredient, id, experience) {
+        return event.custom({
+            type: 'farmersdelight:cooking',
+            result: result,
+            experience: experience,
+            ingredients: ingredient,
+            cookingtime: 200
+        }).id(id) // must give an id!
+    }
 
-    recipes.forEach((recipe) => {
-        event.custom(recipe)
-    })
+    cookingRecipe(
+        Item.of('kubejs:hominy'), [
+            Ingredient.of('corn_delight:corn_seeds'), 
+            Ingredient.of('corn_delight:corn_seeds'), 
+            Ingredient.of('kubejs:lye')
+        ], 
+        'desideri:hominy',
+        0.35
+    )
+    // rooty stew once i figure out how bowl inputs work
 })
