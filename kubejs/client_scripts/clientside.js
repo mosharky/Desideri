6ae6981185fb199245b762c732bb3a274829ad5f
mo_shark <medoelzarkalt@gmail.com>
2022-03-16 12:44:03 -0400

    Clientside script cleanup

--- kubejs/client_scripts/clientside.js
+++ kubejs/client_scripts/clientside.js
@@ -1,14 +1,14 @@
-// Hide items in JEI item list
+// Hide items in REI item list
 onEvent('rei.hide.items', rei => {
 	// rei.hide(global.fullRemovals)
 	rei.hide([
-		// Hides unused BoP wood
-		/(biomesoplenty|compatoplenty|decorative_compat|supplementaries):(.*magic.*|.*umbran.*|.*palm.*)/,
-		// Hides Refined Storage's dyed variants except for red
-		/refinedstorage:.(!red)+_(controller|creative_controller|grid|crafting_grid|pattern_grid|fluid_grid|network_receiver|network_transmitter|relay|wireless_transmitter|disk_manipulator|crafter|crafter_manager|crafting_monitor|security_manager|detector)/,
+		/(biomesoplenty|compatoplenty|decorative_compat|supplementaries):(.*magic.*|.*umbran.*|.*palm.*)/, // tables and beehives aren't caught by this?
+		/refinedstorage:(?!red).+_(controller|creative_controller|.*grid.*|network_receiver|network_transmitter|relay|wireless_transmitter|disk_manipulator|crafter|crafter_manager|crafting_monitor|security_manager|detector)/,
+		/biomesoplenty:potted.*/,
 	])
 })
 
+// REI Information pages
 onEvent('rei.information', rei => {
 	rei.add(global.fullRemovals, Text.of('DISABLED').red().bold())
 })
