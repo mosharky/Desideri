978ea867ae07b0f5e28358f5eb31729f86f17833
mo_shark <medoelzarkalt@gmail.com>
2022-03-17 02:33:49 -0400

    Cooking recipe containers & Rooty Stew recipe

--- kubejs/server_scripts/desideri/mod-specific/farmersdelight/cooking.js
+++ kubejs/server_scripts/desideri/mod-specific/farmersdelight/cooking.js
@@ -1,23 +1,35 @@
 // No bowl inputs?
 onEvent('recipes', (event) => {
-    function cookingRecipe(result, ingredient, id, experience) {
+    function cookingRecipe(ingredients, result, experience, container, id) {
         return event.custom({
             type: 'farmersdelight:cooking',
+            ingredients: ingredients,
             result: result,
             experience: experience,
-            ingredients: ingredient,
+            container: container,
             cookingtime: 200
         }).id(id) // must give an id!
     }
 
-    cookingRecipe(
-        Item.of('kubejs:hominy'), [
-            Ingredient.of('corn_delight:corn_seeds'), 
-            Ingredient.of('corn_delight:corn_seeds'), 
-            Ingredient.of('kubejs:lye')
-        ], 
+    cookingRecipe([
+        Ingredient.of('corn_delight:corn_seeds'),
+        Ingredient.of('corn_delight:corn_seeds'),
+        Ingredient.of('kubejs:lye')
+    ],
+        Item.of('kubejs:hominy'),
+        0.35,
+        Item.of('air'),
         'desideri:hominy',
-        0.35
+
     )
-    // rooty stew once i figure out how bowl inputs work
+    cookingRecipe([
+        Ingredient.of('rootsclassic:old_root'),
+        Ingredient.of('minecraft:wheat'),
+        Ingredient.of('minecraft:beetroot')
+    ],
+        Item.of('rootsclassic:rooty_stew'),
+        0.2,
+        Item.of('minecraft:bowl'),
+        'rootsclassic:rooty_stew'
+    )
 })
