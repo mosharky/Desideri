e088796beb4bc7aa10408fefa999c3a373608e97
mo_shark <medoelzarkalt@gmail.com>
2022-03-26 01:45:22 -0400

    Returned to JEI

--- kubejs/client_scripts/clientside.js
+++ kubejs/client_scripts/clientside.js
@@ -1,23 +1,15 @@
-// Hide items in REI item list
-onEvent('rei.hide.items', rei => {
-	// rei.hide(global.fullRemovals)
-	// blocks that aren't in creative menus aren't getting hidden?
-	// why are tables and beehives not in creative menus???
-	rei.hide([
-		// tables and beehives added by Compat O' Plenty aren't hidden by this?
+// Hide items in JEI item list
+onEvent('jei.hide.items', jei => {
+	// jei.hide(global.fullRemovals)
+	jei.hide([
 		/(biomesoplenty|compatoplenty|decorative_compat|supplementaries):(.*magic.*|.*umbran.*|.*palm.*)/,
-		// this works fine
 		/refinedstorage:(?!red).+_(controller|creative_controller|grid|crafting_grid|pattern_grid|fluid_grid|network_receiver|network_transmitter|relay|wireless_transmitter|disk_manipulator|crafter|crafter_manager|crafting_monitor|security_manager|detector)/,
-		// this doesn't work either
-		/biomesoplenty:potted.*/,
-		// lootr blocks aren't being hidden
-		/lootr:(?!trophy).*/,
 	])
 })
 
-// REI Information pages
-onEvent('rei.information', rei => {
-	rei.add(global.fullRemovals, Text.of('DISABLED').red().bold())
+// JEI Information pages
+onEvent('jei.information', jei => {
+	jei.add(global.fullRemovals, Text.of('DISABLED').red().bold())
 })
 
 // Item tooltips
