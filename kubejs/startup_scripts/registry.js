e0bee6168c7084d9871765ad03c8e57fec7b88ff
mo_shark <medoelzarkalt@gmail.com>
2022-03-29 04:50:45 -0400

    Registered new items (no textures yet)

--- kubejs/startup_scripts/registry.js
+++ kubejs/startup_scripts/registry.js
@@ -7,19 +7,29 @@
 
 onEvent('item.registry', event => {
 
-	// Hemolymph Pustules
 	event.create('hemolymph_pustules', item => {
 		item.displayName('Hemolymph Pustules')
 		item.rarity(RARITY_UNCOMMON)
 	})
 
-	// Lesser Eye of Ender
 	event.create('lesser_ender_eye', item => {
 		item.displayName('Lesser Eye of Ender')
 	})
 
 	event.create('lye', item => {
 		item.displayName('Lye')
+	})
+
+	event.create('crushed_desh_ore', item => {
+		item.displayName('Crushed Desh Ore')
+	})
+
+	event.create('crushed_ostrum_ore', item => {
+		item.displayName('Crushed ostrum Ore')
+	})
+
+	event.create('crushed_calorite_ore', item => {
+		item.displayName('Crushed Calorite Ore')
 	})
 
 	event.create('hominy', item => {
