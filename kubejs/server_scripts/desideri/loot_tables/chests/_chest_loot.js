3104632e3a6f4ed82ea274edcb3994214adb015b
mo_shark <medoelzarkalt@gmail.com>
2022-04-19 08:27:56 -0400

    Refactored `thenReplace()` to `replaceLoot()`

--- kubejs/server_scripts/desideri/loot_tables/chests/_chest_loot.js
+++ kubejs/server_scripts/desideri/loot_tables/chests/_chest_loot.js
@@ -18,15 +18,15 @@ onEvent('lootjs', event => {
     // Remove Flax from common/uncommon urn loot tbales
     event
         .addLootTableModifier(/supplementaries:blocks\/urn_loot\/(common|uncommon)/)
-        .thenReplace('supplementaries:flax_seeds', 'minecraft:string')
-        .thenReplace('supplementaries:rope', '8x farmersdelight:rope')
+        .replaceLoot('supplementaries:flax_seeds', 'minecraft:string')
+        .replaceLoot('supplementaries:rope', '8x farmersdelight:rope')
     // Remove Steel from meteor loot table
     event
         .addLootTableModifier('minecraft:loot')
-        .thenReplace('beyond_earth:steel_ingot', 'immersiveengineering:ingot_steel')
+        .replaceLoot('beyond_earth:steel_ingot', 'immersiveengineering:ingot_steel')
     // Replace Lead & Nickel
     event
         .addLootTableModifier('immersiveengineering:engineers_house')
-        .thenReplace('immersiveengineering:nugget_lead', 'immersiveengineering:nugget_copper')
-        .thenReplace('immersiveengineering:nugget_nickel', 'minecraft:iron_nugget')
+        .replaceLoot('immersiveengineering:nugget_lead', 'immersiveengineering:nugget_copper')
+        .replaceLoot('immersiveengineering:nugget_nickel', 'minecraft:iron_nugget')
 })
