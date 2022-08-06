396053d20b0d8cff65ba77cd71cdd10c6bbc42ce
mo_shark <medoelzarkalt@gmail.com>
2022-08-06 03:06:15 -0400

    re-unified steel in loot tables

--- kubejs/server_scripts/desideri/loot_tables/chests/_chest_loot.js
+++ kubejs/server_scripts/desideri/loot_tables/chests/_chest_loot.js
@@ -19,15 +19,8 @@ onEvent('lootjs', event => {
         .addLootTableModifier(/supplementaries:blocks\/urn_loot\/(common|uncommon)/)
         .replaceLoot('supplementaries:flax_seeds', 'minecraft:string')
         .replaceLoot('supplementaries:rope', '8x farmersdelight:rope')
-    // Remove Steel from meteor loot table
-    /*
+    // replace steel from meteor loot table
     event
         .addLootTableModifier('minecraft:loot')
-        .replaceLoot('beyond_earth:steel_ingot', 'immersiveengineering:ingot_steel')
-    // Replace Lead & Nickel
-    event
-        .addLootTableModifier('immersiveengineering:chests/engineers_house')
-        .replaceLoot('immersiveengineering:nugget_lead', 'oreganized:lead_nugget')
-        .replaceLoot('immersiveengineering:nugget_nickel', 'thermal:nickel_nugget')
-        */
+        .replaceLoot('beyond_earth:steel_ingot', 'alloyed:steel_ingot')
 })
